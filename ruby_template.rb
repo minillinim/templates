e57575d9b8deceb68bbc945cfaee658da5854202
Ben J Woodcroft <donttrustben@gmail.com>
2012-06-27 11:06:41 +1000

    compact logging options so script source code is more readable

--- ruby_template.rb
+++ ruby_template.rb
@@ -2,12 +2,11 @@
 
 require 'optparse'
 require 'bio-logger'
-require 'csv'
 
 SCRIPT_NAME = File.basename(__FILE__); LOG_NAME = SCRIPT_NAME.gsub('.rb','')
 
 # Parse command line options into the options hash
-options = { #TODO set defaults here
+options = {
   :logger => 'stderr',
 }
 o = OptionParser.new do |opts|
@@ -22,25 +21,17 @@ o = OptionParser.new do |opts|
   end
   
   # logger options
-  opts.on("-q", "--quiet", "Run quietly, set logging to ERROR level [default INFO]") do |q|
-    Bio::Log::CLI.trace('error')
-  end
-  opts.on("--logger filename",String,"Log to file [default #{options[:logger]}]") do | name |
-    options[:logger] = name
-  end
-  opts.on("--trace options",String,"Set log level [default INFO]. e.g. '--trace debug' to set logging level to DEBUG") do | s |
-    Bio::Log::CLI.trace(s)
-  end
+  opts.on("-q", "--quiet", "Run quietly, set logging to ERROR level [default INFO]") {Bio::Log::CLI.trace('error')}
+  opts.on("--logger filename",String,"Log to file [default #{options[:logger]}]") { |name| options[:logger] = name}
+  opts.on("--trace options",String,"Set log level [default INFO]. e.g. '--trace debug' to set logging level to DEBUG"){|s| Bio::Log::CLI.trace(s)}
 end
 o.parse!
 if ARGV.length != 0 #TODO require a set number of arguments?
   $stderr.puts o
   exit 1
 end
-# Setup logging
-Bio::Log::CLI.logger(options[:logger]) #bio-logger defaults to STDERR not STDOUT, I disagree
-log = Bio::Log::LoggerPlus.new(LOG_NAME)
-Bio::Log::CLI.configure(LOG_NAME)
+# Setup logging. bio-logger defaults to STDERR not STDOUT, I disagree
+Bio::Log::CLI.logger(options[:logger]); log = Bio::Log::LoggerPlus.new(LOG_NAME); Bio::Log::CLI.configure(LOG_NAME)
 
 
 #TODO what are you looking at me for? This is your script. Do something.
