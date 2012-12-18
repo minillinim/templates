0cc418a03a5856d722c637fce24f6edfa86504f8
Michael.Imelfort <mike@mikeimelfort.com>
2012-12-18 12:45:00 +1000

    modified cause I felt like it

--- python_template.py
+++ python_template.py
@@ -1,64 +1,134 @@
 #!/usr/bin/env python
 ###############################################################################
 #
-#    __Script__Name__
-#    
-#    <one line to give the program's name and a brief idea of what it does.>
-#
-#    Copyright (C) 2012 Michael Imelfort
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# __script_name__.py - description!
 #
 ###############################################################################
+#                                                                             #
+#    This program is free software: you can redistribute it and/or modify     #
+#    it under the terms of the GNU General Public License as published by     #
+#    the Free Software Foundation, either version 3 of the License, or        #
+#    (at your option) any later version.                                      #
+#                                                                             #
+#    This program is distributed in the hope that it will be useful,          #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+#    GNU General Public License for more details.                             #
+#                                                                             #
+#    You should have received a copy of the GNU General Public License        #
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.     #
+#                                                                             #
+###############################################################################
 
-from optparse import OptionParser
+__author__ = "Michael Imelfort"
+__copyright__ = "Copyright 2012"
+__credits__ = ["Michael Imelfort"]
+__license__ = "GPL3"
+__version__ = "0.0.1"
+__maintainer__ = "Michael Imelfort"
+__email__ = "mike@mikeimelfort.com"
+__status__ = "Development"
+
+###############################################################################
+
+import argparse
 import sys
-from pprint import pprint
+
+#import os
+#import errno
+
+#import numpy as np
+#np.seterr(all='raise')     
+
+#import matplotlib as mpl
+#import matplotlib.pyplot as plt
+#from mpl_toolkits.mplot3d import axes3d, Axes3D
+#from pylab import plot,subplot,axis,stem,show,figure
+
 
 ###############################################################################
-# CODE HERE
+###############################################################################
+###############################################################################
 ###############################################################################
 
-def doWork( options ):
+  # classes here
+
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
+
+def doWork( args ):
+    """ Main wrapper"""
+
+    """
+    # parse a file    
+    try:
+        with open(filename, "r") as fh:
+            for line in fh:
+                print line
+    except: 
+        print "Error opening file:", filename, exc_info()[0]
+        raise    
+    """
+
+    """
+    fig = plt.figure()
+
+    #-----
+    # make a 3d plot
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(points[:,0], 
+               points[:,1],
+               points[:,2],
+               #edgecolors='none',
+               #c=colors,
+               #s=2,
+               #marker='.'
+               )
+    
+    #-----
+    # make a 2d plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(points[:,0],
+            points[:,1],
+            '*g')
+
+    #-----
+    # show figure
+    plt.show()  
+    # or save figure
+    plt.savefig(filename,dpi=300,format='png')
+    
+    #-----
+    # clean up!
+    plt.close(fig)
+    del fig
+    """
+
     return 0
 
 ###############################################################################
-# TEMPLATE SUBS
 ###############################################################################
-#
-# Entry point, parse command line args and call out to doWork
-#
+###############################################################################
+###############################################################################
+
 if __name__ == '__main__':
-    # intialise the options parser
-    parser = OptionParser("\n\n %prog [options]")
+
+    parser = argparse.ArgumentParser()
+    #parser.add_argument('positional_arg', help="Required")
+    #parser.add_argument('positional_arg2', type=int, help="Integer argument")
+    #parser.add_argument('positional_arg3', nargs='+', help="Multiple values")
+    #parser.add_argument('-X', '--optional_X', action="store_true", default=False, help="flag")
     
-    # add options here:
-    #parser.add_option("-f", "--frog", action="store_true", dest="frog", default=False, help="Is this a frog? [default: false]")
-    #parser.add_option("-c", "--cat", dest="cat", default=16, help="The cat's age? [default: 16")
-    
-    # get and check options
-    (opts, args) = parser.parse_args()
-    
-    #
-    # compulsory opts
-    #
-    #if (opts.frog is None ):
-    #    print ("**ERROR: %prog : No frog!")
-    #    parser.print_help()
-    #    sys.exit(1)
-    
-    # 
+    # parse the arguments
+    args = parser.parse_args()        
+
     # do what we came here to do
-    #
-    doWork(opts)
+    return doWork(args)
+
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
