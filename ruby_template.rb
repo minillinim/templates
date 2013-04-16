e82a488da4c176756b031e7265e801e31dc8744c
Ben J. Woodcroft <donttrustben near gmail.com>
2013-04-16 11:34:15 +1000

    ruby template changes

--- ruby_template.rb
+++ ruby_template.rb
@@ -3,37 +3,36 @@
 require 'optparse'
 require 'bio-logger'
 
-if __FILE__ == $0 #needs to be removed if this script is distributed as part of a rubygem
-  SCRIPT_NAME = File.basename(__FILE__); LOG_NAME = SCRIPT_NAME.gsub('.rb','')
-  
-  # Parse command line options into the options hash
-  options = {
-    :logger => 'stderr',
-  }
-  o = OptionParser.new do |opts|
-    opts.banner = "
-      Usage: #{SCRIPT_NAME} <arguments>
-      
-      Description of what this program does...\n\n"
-      
-    opts.on("-e", "--eg ARG", "description [default: #{options[:eg]}]") do |arg|
-      options[:example] = arg
-    end
+SCRIPT_NAME = File.basename(__FILE__); LOG_NAME = SCRIPT_NAME.gsub('.rb','')
 
-    # logger options
-    opts.separator "\nVerbosity:\n\n"
-    opts.on("-q", "--quiet", "Run quietly, set logging to ERROR level [default INFO]") {Bio::Log::CLI.trace('error')}
-    opts.on("--logger filename",String,"Log to file [default #{options[:logger]}]") { |name| options[:logger] = name}
-    opts.on("--trace options",String,"Set log level [default INFO]. e.g. '--trace debug' to set logging level to DEBUG"){|s| Bio::Log::CLI.trace(s)}
-  end; o.parse!
-  if ARGV.length != 0
-    $stderr.puts o
-    exit 1
+# Parse command line options into the options hash
+options = {
+  :logger => 'stderr',
+  :log_level => 'info',
+}
+o = OptionParser.new do |opts|
+  opts.banner = "
+    Usage: #{SCRIPT_NAME} <arguments>
+    
+    Description of what this program does...\n\n"
+    
+  opts.on("-e", "--eg ARG", "description [default: #{options[:eg]}]") do |arg|
+    options[:example] = arg
   end
-  # Setup logging. bio-logger defaults to STDERR not STDOUT, I disagree
-  Bio::Log::CLI.logger(options[:logger]); log = Bio::Log::LoggerPlus.new(LOG_NAME); Bio::Log::CLI.configure(LOG_NAME)
-  
-  
-  #TODO what are you looking at me for? This is your script. Do something.
-  log.info 'e.g. logging'
-end #end if running as a script
+
+  # logger options
+  opts.separator "\nVerbosity:\n\n"
+  opts.on("-q", "--quiet", "Run quietly, set logging to ERROR level [default INFO]") {options[:log_level] = 'error'}
+  opts.on("--logger filename",String,"Log to file [default #{options[:logger]}]") { |name| options[:logger] = name}
+  opts.on("--trace options",String,"Set log level [default INFO]. e.g. '--trace debug' to set logging level to DEBUG"){|s| options[:log_level] = s}
+end; o.parse!
+if ARGV.length != 0
+  $stderr.puts o
+  exit 1
+end
+# Setup logging
+Bio::Log::CLI.logger(options[:logger]); Bio::Log::CLI.trace(options[:log_level]); log = Bio::Log::LoggerPlus.new(LOG_NAME); Bio::Log::CLI.configure(LOG_NAME)
+
+
+#TODO what are you looking at me for? This is your script. Do something.
+log.info 'e.g. logging'
