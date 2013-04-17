a567fbf085c730123450e3104ec419bd0024bd88
Ben J. Woodcroft <donttrustben near gmail.com>
2013-04-17 10:02:49 +1000

    learned about python testing a bit more

--- python_test_template.py
+++ python_test_template.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 #=======================================================================
 # Author:
 #
@@ -5,7 +7,7 @@
 #
 # Copyright
 #
-# STAMP is free software: you can redistribute it and/or modify
+# This is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
@@ -21,22 +23,26 @@
 
 import unittest
 import sys
+import subprocess
+import tempfile
+import os.path
 
 
-class VerifyPostHocTests(unittest.TestCase):
-	def testGamesHowell(self):
-		"""Verify computation of Games-Howell post-hoc test"""
-		from plugins.multiGroups.postHoc.GamesHowell import GamesHowell
-		gh = GamesHowell(preferences)
+class Tests(unittest.TestCase):
+  def testSomething(self):
+    bamOutputFile = '/tmp/a'+'.bam'
+    subprocess.check_call(path_to_script+' -1 data/reads.1.fq.gz -2 data/reads.2.fq.gz -d data/ref.fna -b '+bamOutputFile, shell=True)
 
-		# ground truth found with SPSS v19. Values are not exact since the critical Q value
-		# are interpolated from tables in the STAMP implementation.
-		pValues, effectSize, lowerCI, upperCI, labels, note = gh.run([[1,2,3,4,5],[10,20,30,40,50,60],[1,2,3,4,5,6,7]], 0.95, ['1', '2', '3'])
-		self.assertEqual(labels[0], '1 : 2')
-		self.assertAlmostEqual(effectSize[0], -32)
-		self.assertAlmostEqual(lowerCI[0], -56.836534205367272) # SPSS = -56.80902338101632
-		self.assertAlmostEqual(upperCI[0],  -7.163465794632728) # SPSS = -7.190976618983683
-		self.assertEqual(pValues[0] == '< 0.02', True) # SPSS = 0.019165308600281317
+    samOutput = subprocess.check_output('samtools view '+bamOutputFile, shell=True)
+
+    expectedSamOutput = 'FCC0WM1ACXX:2:2106:7087:17037#TATCCAGA	89	E1D_contig_2582764	1477	37	100M	=	1477	0	GCAGTCACTGCACCGCAAGGAAGCACAGAACCTGCAATGATATTTCACATCGTGGGACTTAAATATCTTTTGGCATTGTTCACACTTGGGCTTTTCCTTC	cccccbbccccaccdddeeeeeeggfgggchiihiiihihfhffihgiihihhgghhhfiiihiiiiiihiihfiiihiihgihiiigggggeeeeebbb	XT:A:U	NM:i:0	SM:i:37	AM:i:0	X0:i:1	X1:i:0	XM:i:0	XO:i:0	XG:i:0	MD:Z:100'+"\n"
+    expectedSamOutput += 'FCC0WM1ACXX:2:2308:9246:128896#TATCCAGA	89	E1D_contig_3	23	37	100M	=	23	0	AATACAACGGAATCCGCTTTCTCCCGCTGAAGCTGGGACGCTGGCTTAACGACTTCGACGAATCTCAAAAACGCAACGTCGTCGTTCTCGGCGATGAGAT	ccccccccdccccccccb^Ta__accb_cbbabcaccacc_bbbbaccccb^cccccdeeeeeggeiihg`hiihgggaihhhiiihgfgggeeeeebbb	XT:A:U	NM:i:0	SM:i:37	AM:i:0	X0:i:1	X1:i:0	XM:i:0	XO:i:0	XG:i:0	MD:Z:100'+"\n"
+
+    self.assertEqual(expectedSamOutput, samOutput)
+
+    # Check the index exists also
+    self.assertEqual(True, os.path.isfile(bamOutputFile+'.bai'))
+
 
 if __name__ == "__main__":
 	unittest.main()
